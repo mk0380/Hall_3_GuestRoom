53d1d6691751f20918b1f92c7b43f324f80b1574
Mayank Kumar <mayankkumar03808766@gmaile.com>
2024-06-21 23:26:01 +0530

    bck image resolved

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -3,13 +3,16 @@ const {
   db_connection_failed,
   db_url_not_defined_error,
 } = require("@/important_data/important_data");
-const responseHandler = require("./responseHandler");
 const mongoose = require("mongoose");
 
 const connectDB = async (res) => {
   if (!process.env.MONGODB_URL) {
     console.error("MongoDB URL is not defined in environment variables.");
-    return responseHandler(res, false, 503, db_url_not_defined_error);
+    // return responseHandler(res, false, 503, db_url_not_defined_error);
+    return {
+      success: false,
+      message: db_url_not_defined_error
+    }
   }
 
   try {
@@ -17,13 +20,24 @@ const connectDB = async (res) => {
 
     if ((connection?.connection?.readyState ?? 0) !== 1) {
       console.error("Database connection failed: Not ready.");
-      return responseHandler(res, false, 503, db_connection_failed);
+      // return responseHandler(res, false, 503, db_connection_failed);
+      return {
+        success: false,
+        message: db_connection_failed
+      }
     }
 
-    return;
+    return {
+      success: true,
+      message: ""
+    }
   } catch (error) {
     console.error(`Database connection error: ${error.message}`);
-    return responseHandler(res, false, 503, db_connection_error);
+    // return responseHandler(res, false, 503, db_connection_error);
+    return {
+      success: false,
+      message: db_connection_error
+    }
   }
 };
 
