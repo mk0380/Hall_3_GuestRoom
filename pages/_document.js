e5fee2f9a7ed7c6f65b556e038b9c7bddf471f88
Mayank Kumar <mayankkumar03808766@gmaile.com>
2024-06-23 16:19:53 +0530

    analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="ga-script" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+          });
+        `}
+        </Script>
         <meta
           name="description"
           content="Welcome to the Guest Room Booking Portal for Hall of Residence 3 at IIT Kanpur. Book comfortable rooms for your stay, conveniently located within the campus. Experience a relaxing stay with our well-furnished rooms and convenient amenities."
